ff676e010a6808230b35d555a86de49250f475f9
David Tei <daveteidt@gmail.com>
2023-07-25 05:48:28 +0000

    Modify gather_data_from_an_API

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A Python script that returns information about a given employee ID's
+TODO list progress and export the data in the CSV format."""
+
+import sys
+import requests
+import csv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todo = requests.get(url + "todo", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todo]
