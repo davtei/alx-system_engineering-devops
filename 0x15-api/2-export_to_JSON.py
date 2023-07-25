d4114774c0ad5713bd66fb544a8d02cc262fce50
David Tei <daveteidt@gmail.com>
2023-07-25 05:53:50 +0000

    Add export_to_JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A Python script that returns information about a given employee ID's
+TODO list progress and export the data in the JSON format."""
+
+import sys
+import requests
+import json
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
