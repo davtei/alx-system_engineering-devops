9ccea909d9785e208fc645be4994d5c68e18105a
David Tei <daveteidt@gmail.com>
2023-08-09 06:21:50 +0000

    Add top_ten script

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,11 +7,15 @@ def top_ten(subreddit):
     """A function that queries the Reddit API and prints the titles of the
     first 10 hot posts listed for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    params = {"limit": 10}
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        }
+    params = {
+        "limit": 10
+        }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
     results = response.json().get("data")
-    [print(c.get("title")) for c in results.get("children")]
+    [print(c.get("data").get("title")) for c in results.get("children")]
