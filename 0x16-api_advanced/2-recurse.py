73097af588e24255d018c925e38ef93baaf415a5
David Tei <daveteidt@gmail.com>
2023-08-09 06:02:46 +0000

    Add recurse script

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A Python script that queries the Reddit API using request module."""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """A recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit. If no
+    results are found for the given subreddit, the function should return
+    None."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+        }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return ("None")
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
